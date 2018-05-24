fa688be968ea67055f50b537ce7dc054fd0b8867
piiok <33143055+piiok@users.noreply.github.com>
2018-05-24 10:14:06 -0500

    Ofrecer Ruta, falta conectar con la bd

--- O_ruta1.php
+++ O_ruta1.php
@@ -8,10 +8,10 @@
   </head>
   <body>
     <script>
-      var cant = 0;
+      var cant = 0; //Contador de escalas
     </script>
     <center>
-      <form action="O_ruta2.php" method="post" accept-charset="utf-8">
+      <form action="O_ruta_db.php" method="post" accept-charset="utf-8">
       
         <br>
         <br>
@@ -23,32 +23,81 @@
         Cantidad de cupos disponibles: <input type="number" name="cupos"><br><br><br>
         Origen: 
         <select name="TipoOrigen">
-          <option value="lugar"  checked>Lugar</option>
-          <option value="cra">Cra</option>
+          <option value="lugar"  >Lugar</option>
+          <option value="cra" selected="true">Cra</option>
           <option value="cll">Calle</option>
-          <option value="direccion">Dirección</option>
           <option value="Barrio">Barrio</option>
         </select> 
-        <div id="cont"></div>
+        <input type="text" name="Origen">
+        <br>
+        <hr style="width: 60vw;"> 
+        Destino: 
+        <select name="TipoDestino">
+          <option value="lugar"  >Lugar</option>
+          <option value="cra" selected="true">Cra</option>
+          <option value="cll">Calle</option>
+          <option value="Barrio">Barrio</option>
+        </select> 
+        <input type="text" name="Destino">
+        <br>
+        <hr style="width: 60vw;">
+        Escalas <br>
+        <div id="escalas"> 
+          <div id="1" name="holi">
+            
+          </div>
+          <!-- acá se irán agregando las escalas gracias al botón agregar parada -->
+        </div>
+        <br>
+        <input type="button" onclick="agregarEscala()" value="Agregar Parada"><br><br>
 
         <button class="continuar" type="submit">Continuar</button>
       </form>
     </center>
     <script>
-      var f=new Date(); 
-      document.getElementById("HPartida").value=f.getHours()+":"+f.getMinutes();
-      var x=f.getMonth()+1;
-      if (x<10) {
-        x="0"+x;
-      }
-      var y=f.getDate();
-      if (y<10) {
-        y="0"+y;
-      }
+
+      var f=new Date(); //Este objeto contiene la hora y fecha del momento en que se crea
+
+      //Arreglamos la hora y los minutos para que el type time lo reciba bn, y luego se lo pasamos. Esto le asigna al input la hora actual
+      var h = f.getHours()<10?"0"+f.getHours():f.getHours(); //if comprimido si cumple esto "?" entonces pasa esto si no ":" pasa lo otro
+      var m = f.getMinutes()<10?"0"+f.getMinutes():f.getMinutes();
+      document.getElementById("HPartida").value=h+":"+m;
+
+      //Arreglamos el mes y el día para que el type date lo reciba bn, y luego se lo pasamos. Esto le asigna al input la fecha actual
+      var x=(f.getMonth()+1)<10?"0"+(f.getMonth()+1):f.getMonth()+1;
+      var y=f.getDate()<10?"0"+f.getDate():f.getDate();
       document.getElementById("Date").value = f.getUTCFullYear()+"-"+x+"-"+y;
-      //document.getElementById("HPartida").value=f.getHours()+":"+f.getMinutes();
-      var tipo = document.getElementById("cont");
-      
+
+      //funciones
+
+      function agregarEscala(){
+        divE=document.getElementById("escalas"); //div en donde van las escalas
+        divE.innerHTML+="<br>"; //se agrega un enter para que las lineas no queden tan pegadas
+
+        //Agregando un nuevo select
+
+        var inp = document.createElement('select'); //Se crea un select
+        inp.name="tipoEscala["+cant+"]"; //Se le asigna nombre
+        inp.id="tipoEscala["+cant+"]"; //Se le asigna id
+        divE.appendChild(inp); //Se agrega al div 
+
+
+        //Agregando los option al select
+
+        document.getElementById(inp.id).innerHTML+=
+          "<option value='lugar'>Lugar</option>\n<option value='cra' selected='true'>Cra</option>\n<option value='cll'>Calle</option>\n<option value='Barrio'>Barrio</option>";
+        divE.innerHTML+=" \n";
+
+        //Agregando el input type text, muy similar al anterior
+        var inp2 = document.createElement('input');
+        inp2.type="text";
+        inp2.name="Escala["+cant+"]";
+        inp2.id="Escala["+cant+"]";
+        divE.appendChild(inp2);
+
+        divE.innerHTML+="<br>"; //Para que no quede tan pegado jiji
+        cant+=1;//Aumentamos la cantidad de escalas existentes
+      }
     </script>
    
     
