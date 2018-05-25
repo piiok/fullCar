0420176e2f1d9591ffb32b0fb27242e71cdca2b0
piiok <33143055+piiok@users.noreply.github.com>
2018-05-24 22:07:19 -0500

    Base de datos completada

--- O_ruta_db.php
+++ O_ruta_db.php
@@ -13,16 +13,34 @@
 		session_start();
 		extract($_POST);
 
-		$cons="UPDATE full_car.vehiculo SET t_propiedad ='".$t_propiedad_v."', t_mecanico = '".$t_mecanico_v."' WHERE cel_v='".$_SESSION[cel]."';";
-
-		#$res = mysqli_query ($conn ,$cons);
+		$cons="INSERT INTO full_car.ruta(fecha,hora_salida,hora_llegada,origen,origen_tipo,destino,destino_tipo,cel_r) VALUES ('".$Date."','".$HPartida."','".$HLlegada."','".$Origen."','".$TipoOrigen."','".$Destino."','".$TipoDestino."','".$_SESSION[cel]."');";
+		echo $cons;
+		$res = mysqli_query ($conn ,$cons);
 		if(empty($res)){
- 			echo 'Error al registrarse'.mysqli_error($conn);
+ 			echo 'Error: '.mysqli_error($conn);
 		}else{
-
+			$cons1="SELECT id_ruta FROM full_car.ruta ORDER BY id_ruta desc";
+			$rest = mysqli_query ($conn ,$cons1);
+			$id_ruta=$rest->fetch_array()[0]; #Machetico chan
+			print_r($tipoEscala);
+			print_r($Escala);
+			$i=0;
+			$tipo=$tipoEscala[0];
+			echo $tipo;
+			while (!empty($tipo)) {
+				if($Escala[$i]!=""){
+					$consi="INSERT INTO full_car.escalas(lugar,lugar_tipo,id_ruta_e) VALUES ('".$Escala[$i]."','".$tipo."','".$id_ruta."');";
+					$resti = mysqli_query($conn ,$consi);
+					if(empty($resti)){
+			 			echo 'Error al registrarse'.mysqli_error($conn);
+					}
+				}		
+				$i+=1;
+				$tipo=$tipoEscala[$i];
+			}
 		}
 		$conn->close();
-		header("Location:./home.php");
+		header("Location:./OfrecerRutaLoad.html");
 
  	?>
 </body>
