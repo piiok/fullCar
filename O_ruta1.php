7764e385b6a02078f361a4784c11bb1fefad7aca
hectorgonzalez13 <tayzon1392@hotmail.com>
2018-05-24 22:20:35 -0500

    Estilo Ofrecer Ruta

--- O_ruta1.php
+++ O_ruta1.php
@@ -1,58 +1,91 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="UTF-8">
-    <link rel="shortcut icon" href="imagenes/icon.ico" type="image/x-icon" />
-    <title>Ofrecer Ruta</title>
+    <title>Ofrecer ruta</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="./imagenes/icon.ico" type="image/x-icon" />
+    <link rel="stylesheet" type="text/css" href="./css/registrarse.css">
+    <link rel="stylesheet" href="https://bootswatch.com/4/darkly/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
   </head>
   <body>
     <script>
       var cant = 0; //Contador de escalas
     </script>
-    <center>
-      <form action="O_ruta_db.php" method="post" accept-charset="utf-8">
+
+      
+
+    <center><div class="col-xl-12 text-left" class="col-lg-6 text-left" class="col-md-6 text-left" class="col-sm-6 text-left" style="padding-top: 15px; min-height: 100vh; display: grid;"> 
+    <center><form class="formulario" action="O_ruta_db.php" method="post" accept-charset="utf-8">
+  <fieldset>
+    
+
+    
       
         <br>
         <br>
-        <h3>Ofrecer Ruta</h3> <br>
+        <h1>Ofrecer Ruta</h1> <br>
+        <label for="exampleInputEmail1">Fecha de partida:</label>
+        <input class="form-control col-6" type="date" id="Date" name="Date" onchange=""><br>
+        <label for="exampleInputEmail1">Hora de partida:</label>
+        <input class="form-control col-5" type="time" id="HPartida" step="hh:mm" name="HPartida" > <br>
+        <label for="exampleInputEmail1">Hora de estimada de llegada:</label>
+        <input class="form-control col-5" type="time" name="HLlegada" id="HLlegada" value="12:00"> <br>
+        <!-- ############### -->
+        
+       <label for="exampleInputEmail1"> Cantidad de cupos disponibles:</label>
         <br>
-        Fecha de partida: <input type="date" id="Date" name="Date" onchange=""><br><br><br>
-        Hora de partida: <input type="time" id="HPartida" step="hh:mm" name="HPartida" > <br><br><br>
-        Hora estimada de llegada: <input type="time" name="HLlegada" id="HLlegada" value="12:00"> <br><br><br>
-        Cantidad de cupos disponibles: <input type="number" name="cupos"><br><br><br>
-        Origen: 
+        <div class="col-3">
+        <input class="form-control" type="number" name="cupos" placeholder="0">
+        </div>
+        <!-- ############# -->
+        <!-- Cantidad de cupos disponibles: <input type="number" name="cupos"><br><br> -->
+        <br>
+        <label for="exampleInputEmail1">Origen:</label> 
         <select name="TipoOrigen">
-          <option value="lugar"  >Lugar</option>
+          <option value="lugar">Lugar</option>
           <option value="cra" selected="true">Cra</option>
           <option value="cll">Calle</option>
           <option value="Barrio">Barrio</option>
         </select> 
-        <input type="text" name="Origen">
-        <br>
-        <hr style="width: 60vw;"> 
-        Destino: 
+        <input class="form-control col-5" type="text" name="Origen" placeholder="Ejemplo 25 # 14-61">
+
+        <hr style="width: 60vw;">
+
+        <label for="exampleInputEmail1">Destino:</label> 
         <select name="TipoDestino">
           <option value="lugar"  >Lugar</option>
           <option value="cra" selected="true">Cra</option>
           <option value="cll">Calle</option>
           <option value="Barrio">Barrio</option>
         </select> 
-        <input type="text" name="Destino">
-        <br>
+        <input class="form-control col-5" type="text" name="Destino" placeholder="Ejemplo 33 # 36-43">
+
         <hr style="width: 60vw;">
-        Escalas <br>
+        <label for="exampleInputEmail1" style="color: black;">Escalas:</label> 
         <div id="escalas"> 
           <div id="1" name="holi">
             
           </div>
           <!-- acá se irán agregando las escalas gracias al botón agregar parada -->
         </div>
-        <br>
-        <input type="button" onclick="agregarEscala()" value="Agregar Parada"><br><br>
+        <!-- ################### -->
 
-        <button class="continuar" type="submit">Continuar</button>
-      </form>
+        <button type="button" class="btn btn-primary btn-sm" onclick="agregarEscala()"style="border: solid 0.5px black;">Agregar parada</button>
+
+        <!-- ################## -->
+        <!-- <input type="button" onclick="agregarEscala()" value="Agregar Parada"> -->
+        <br><br>
+        
+
+        <button class="continuar btn btn-warning btn-lg btn-large" type="submit" style="margin-bottom: 15px;border: solid 0.5px black;">Continuar</button>
+        <!-- <button class="continuar" type="submit" style="margin-bottom: 20px;">Continuar</button> -->
+      </fieldset>
+      </form></center>
+    </div>
     </center>
     <script>
 
@@ -99,8 +132,5 @@
         cant+=1;//Aumentamos la cantidad de escalas existentes
       }
     </script>
-   
-    <br>
-    <br>
   </body>
 </html>
